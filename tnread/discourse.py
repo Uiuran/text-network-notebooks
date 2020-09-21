d1b4884102bf87d95ceb3209157f3852baca9179
uiuran <dkajah@gmail.com>
2020-09-20 22:11:44 -0300

    maximal interjunction

--- tnread/discourse.py
+++ tnread/discourse.py
@@ -41,7 +41,23 @@ def set_edges_by_nodes(textnet):
             edge[2]['roles'].append('diffusor')
 
         elif (partition[edge[0]] != partition[edge[1]]):
-            edge[2]['roles'].append('inter communities') 
+            edge[2]['roles'].append('inter communities')
+
+def maximal_interjunction(textnet, num=2):
+    '''
+    Get num inter junctions edges with highest weight for each junction node
+    '''
+    max_w = []
+    for node,config in vis.textnet.finalGraph.nodes.items():
+        weights = dict()
+        if 'junction' in config['roles']:
+            for neigh in vis.textnet.finalGraph.neighbors(node):
+                if 'inter junction' in vis.textnet.finalGraph.get_edge_data(node,neigh)['roles']:
+                    weights[(node,neigh)]=vis.textnet.finalGraph.get_edge_data(node,neigh)['weight']
+            weights = {k:v for k,v in sorted(weights.items(),key=lambda x: x[1],reverse=True)}
+            max_w.extend(list(weights.keys())[0:num])
+    return max_w
+
 
 def get_hubs(textnet, community=0):
     '''
