c446d5ad4d8fbbf9ad6fd72ada93b60e76ce1628
Daniel Penalva <dkajah@gmail.com>
2020-09-23 21:50:00 -0300

    Update discourse.py
    deletions i forget in the previous commit

--- tnread/discourse.py
+++ tnread/discourse.py
@@ -1,23 +1,5 @@
 from .vis import partitiontodict
 
-def set_junctions_hubs(textnet, how_many_junct = 3, how_many_hubs = 3):
-    '''
-     Extracts nodes inside communities with higher betweeness centrality and connectivity.
-     Setup node role information in given textnet.
-    
-    '''
-
-    setattr(textnet,'n_hub',how_many_hubs)
-    setattr(textnet,'n_junction',how_many_junct)
-
-    for com,df in textnet.bc_top_all.groupby('community'):
-        jnames=df.sort_values(['bc_norm'],axis=0,ascending=False)['node'].iloc[0:how_many_junct]
-        hnames=df.sort_values(['degree'],axis=0,ascending=False)['node'].iloc[0:how_many_hubs]
-        for name in jnames:
-            textnet.cutoffUnGraph.nodes[name]["roles"].append("junction")
-        for name in hnames:
-            textnet.cutoffUnGraph.nodes[name]["roles"].append("hub")
-
 def setup_roles(textnet, how_many_junct = 3, how_many_hubs = 3):
     '''
      Extracts nodes inside communities with higher betweeness centrality and connectivity.
@@ -91,17 +73,3 @@ def max_path(textnet,edge_function=['inter junction'], num=2):
             max_w.update({k:(1.0,0.0,0.0,vals[0]) for k,v in zip(keys,vals)})
 
     return max_w
-
-def _hubs(textnet, community=0):
-    '''
-     Get all hub names from community (must be an integer, if its none get all hubs)
-     Hubs are the nodes with highest degree.
-    '''
-    pass
-
-def get_junctions(textnet, community=0):
-    '''
-     Get all junction names from community (must be an integer, if its none get all junctions).
-     Junctions are the nodes with highest betweeness-centrality.
-    '''
-    pass
